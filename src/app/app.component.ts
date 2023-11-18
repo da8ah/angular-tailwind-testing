61aedb6a79f88731c6c74ba098479821f43da965
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-11-17 23:45:47 -0500

    nav: material sidenav setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-tailwind-testing';
+
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+  constructor(private observer: BreakpointObserver, private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true
+    this.observer.observe(['(max-width:787px)'])
+      .subscribe((res) => {
+        if (res?.matches) {
+          this.sideNav.mode = "over"
+          this.sideNav.close()
+        } else {
+          this.sideNav.mode = "side"
+          this.sideNav.close()
+        }
+        this.cdr.detectChanges()
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
